dbc88631b947d9e6f36d317d69d8c448d335b2e3
Paula-Elorrieta <paula.fragapi@elorrieta-errekamari.com>
2024-10-09 12:28:37 +0200

    09/10 clase

--- Angular_Ariketak_2/GifsApp/src/app/gifs/busqueda/busqueda.component.ts
+++ Angular_Ariketak_2/GifsApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -17,6 +17,24 @@ export class BusquedaComponent {
 
   buscar() {
     console.log(this.valor);
+
+    // Si el valor es vacío no se hace nada
+    if (this.valor.trim().length === 0) {
+      return alert('No se puede buscar un valor vacío');
+    }
+
+    // El valor que se guarda tiene que ser en minusculas y sin espacios
+    this.valor = this.valor.toLowerCase();
+    if (this.valor.trim().length > 0) {
+      // En el replace /\s/g es una expresión regular que busca todos los espacios en blanco y los reemplaza por nada
+      this.valor = this.valor.replace(/\s/g, '');
+    }
+
+    // No puede haber valores repetidos
+    if (this.gifsService.historial.includes(this.valor)) {
+      return alert('No puedes hacer búsquedas repetidas');
+    }
+
     this.gifsService.buscarGifs(this.valor);
     this.valor = '';
   }
--- Angular_Ariketak_2/GifsApp/src/app/gifs/service/gifs.service.ts
+++ Angular_Ariketak_2/GifsApp/src/app/gifs/service/gifs.service.ts
@@ -1,20 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  constructor(private httpClien: HttpClient) { }
+
   private _historial: string[] = [];
+  gifs : any[] = [];
 
   get historial() {
     // Los tres puntos son para romper la referencia con el historial original
     return [...this._historial];
   }
 
+
   buscarGifs (busqueda : string) {
+    // Si el array ya tiene 10 elementos se elimina el último
+    if (this._historial.length >= 10) {
+      this._historial.pop();
+    }
+
     this._historial.unshift(busqueda);
     console.log(this._historial);
+
+
+    this.httpClien.get("https://api.giphy.com/v1/gifs/search?api_key=nuy8WMynQh8AQFNtZ0ZA74upxyjFeGD1&q="+busqueda+"&limit=10").subscribe
+    ((resp ) => {console.log(resp);});
+
+
   }
+
+  
+  
+
+
+      
+
+
   
 }
