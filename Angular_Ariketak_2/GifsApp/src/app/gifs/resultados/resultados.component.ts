928c9d425f7f07b936d227f42ffdfea916fa72c3
Paula-Elorrieta <paula.fragapi@elorrieta-errekamari.com>
2024-10-10 08:48:24 +0200

    10/10 Clase

--- Angular_Ariketak_2/GifsApp/src/app/gifs/resultados/resultados.component.ts
+++ Angular_Ariketak_2/GifsApp/src/app/gifs/resultados/resultados.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { GifsService } from '../service/gifs.service';
 
 @Component({
   selector: 'app-resultados',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './resultados.component.html',
   styleUrl: './resultados.component.css'
 })
 export class ResultadosComponent {
+
+  constructor(private gifsService : GifsService ) {}
+
+  get gifs() {
+    return this.gifsService.gifs;
+  }
+
 
 }
--- Angular_Ariketak_2/GifsApp/src/app/gifs/service/gifs.service.ts
+++ Angular_Ariketak_2/GifsApp/src/app/gifs/service/gifs.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { gifsInterface } from '../interface/IGifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,8 @@ export class GifsService {
   constructor(private httpClien: HttpClient) { }
 
   private _historial: string[] = [];
-  gifs : any[] = [];
+  //gifs : any[] = [];
+  gifs : gifsInterface[] = [];
 
   get historial() {
     // Los tres puntos son para romper la referencia con el historial original
@@ -28,10 +30,11 @@ export class GifsService {
     console.log(this._historial);
 
 
-    this.httpClien.get("https://api.giphy.com/v1/gifs/search?api_key=nuy8WMynQh8AQFNtZ0ZA74upxyjFeGD1&q="+busqueda+"&limit=10").subscribe
-    ((resp ) => {console.log(resp);});
-
-
+    this.httpClien.get("https://api.giphy.com/v1/gifs/search?api_key=nuy8WMynQh8AQFNtZ0ZA74upxyjFeGD1&q="+busqueda+"&limit=20").subscribe
+      ((resp : any ) => {
+      console.log(resp.data);
+        this.gifs = resp.data;
+    });
   }
 
   
