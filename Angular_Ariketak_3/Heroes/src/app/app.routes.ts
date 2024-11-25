f1ea5ce0bb94be45d433cfcd718907747c55964f
Paula Fraga <paula.fragapi@elorrieta-errekamari.com>
2024-11-25 09:23:43 +0100

    Me pide commit

---
+++ Angular_Ariketak_3/Heroes/src/app/app.routes.ts
@@ -0,0 +1,30 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/pages/login/login.component';
+import { RegistroComponent } from './auth/pages/registro/registro.component';
+import { ListadoComponent } from './heroes/pages/listado/listado.component';
+import { AgregarComponent } from './heroes/pages/agregar/agregar.component';
+import { BuscarComponent } from './heroes/pages/buscar/buscar.component';
+import { HeroeComponent } from './heroes/pages/heroe/heroe.component';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+export const routes: Routes = [
+    {
+        path: 'auth', children: [
+            { path: 'login', component: LoginComponent },
+            { path: 'registro', component: RegistroComponent },
+            { path: '**', redirectTo: 'login' }
+        ]
+    },
+    {
+        path: 'heroes', children: [
+            {path: 'listado', component: ListadoComponent},
+            {path: 'agregar', component: AgregarComponent},
+            {path: 'editar/:id', component: AgregarComponent},
+            {path: 'buscar', component: BuscarComponent},
+            {path: ':id', component: HeroeComponent},
+            {path: '**', redirectTo: 'listado'}
+        ]
+    },
+    { path: '404', component: ErrorPageComponent },
+    { path: '**', redirectTo: '404' },
+];
